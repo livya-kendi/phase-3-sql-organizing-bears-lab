4ef95d72d2a1e5661d1482d3055e9eeca5c10879
Livya Kendi Kiruja <livyakendi1@gmail.com>
2022-09-01 07:08:54 +0300

    done

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 20, "M", "Brown", "Calm", "1");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 10, "F", "Black", "Calm", "0");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 6, "M", "White", "Calm", "1");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 1, "F", "Red", "Calm", "0");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 13, "M", "Green", "Calm", "1");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 2, "F", "Orange", "Calm", "0");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 6, "M", "Blue", "Calm", "1");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 4, "M", "Yellow", "Calm", "0");
+
